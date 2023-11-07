44aca0e620140c2164dcaf4b3311d0468dd3da61
Nelson Carias <Carias030@outlook.com>
2023-11-07 14:27:21 -0600

    Portafolio

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <MiPortafolio></MiPortafolio>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MiPortafolio from './components/MiPortafolio.vue'
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/components/MiPortafolio.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="">
+        <div class="">
+            <p class="flex justify-center text-2xl p-5"><b>Hello! 🖐 I'm a software developer</b></p>
+            <br>
+            <div class="flex justify-center">
+                <img  width="200" height="300" src="https://mattfarley.ca/img/mf-avatar.svg">                
+            </div>
+            <br>
+            <div class="flex justify-center">
+                <img  width="600" src="https://mattfarley.ca/img/hero-devices.svg">
+            </div>
+            <div  class="bg-indigo-600">
+                <div>
+                    <p  class="text-white text-center text-2xl flex justify-center p-5">Hi, I’m Nelson Carías. Nice to meet you.</p>
+                    <br>
+                    <p  class="text-white text-center flex justify-center text-xl">I am a student, eager to learn and improve myself on a professional and personal level.<br>
+                                                                            I am also currently a professional internship intern at Seed Code SV where I have gained more <br>
+                                                                            experience in new technologies and development tools.
+                                                                            For me this is the beginning<br> of a lot of experience and knowledge that I will obtain as a professional.</p>
+                </div>
+                <br>
+                <br>
+                <div class="rounded-lg bg-white ml-32 mr-32">
+                    
+                        <p class="flex justify-center text-2xl p-5"><b>🔧 Technologies and Tools</b></p>
+                        <hr>
+                        <div class="grid grid-cols-8 p-10">
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8 " src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Microsoft_SQL_Server-CC2927?style=for-the-badge&logo=microsoft-sql-server&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"/>
+                                </a>
+                               
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"/>
+                                </a>
+
+
+
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Microsoft_SQL_Server-CC2927?style=for-the-badge&logo=microsoft-sql-server&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"/>
+                                </a>
+                               
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"/>
+                                </a>
+                                <a class="p-2">
+                                    <img class=" h-8" src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"/>
+                                </a>
+                        </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+    
+    <div class="bg-indigo-600">
+                <div class="">
+                    
+                    <!-- <div>
+                        <button>Facebook</button>
+                        <button>Instagram</button>
+                        <button>Whatsapp</button>
+                    </div> -->
+                </div>
+                
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
